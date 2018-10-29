7b655508a486733a1e64af827a5c8d2cacb85acd
joshjinlee <joshua_jin_lee@live.com>
2018-10-29 14:34:23 -0400

    finished contacts page with responsiveness

--- js/main.js
+++ js/main.js
@@ -1,79 +1,94 @@
 
-var testing = false;
+var testing = true;
 
 var home = {
-		path: "#home",
-		container: ".home-container",
-		back: {
-			path: "#contact",
-			container: ".contact-container",},
-		forw: {
-			path: "#skills",
-			container: ".skills-container"}}
-
-var skills = {
-		path: "#skills",
-		container: ".skills-container",
-		back: {
-			path: "#home",
-			container: ".home-container",},
-		forw: {
-			path: "#work",
-			container: ".work-container"}}
-
-var work ={
-		path: "#work",
-		container: ".work-container",
-		back: {
-			path: "#skills",
-			container: ".skills-container",},
-		forw: {
-			path: "#projects",
-			container: ".projects-container"}}
-
-var projects = {
-		path: "#projects",
-		container: ".projects-container",
-		back: {
-			path: "#work",
-			container: ".work-container",},
-		forw: {
-			path: "#contact",
-			container: ".contact-container"}}
-
-var contact ={
+	path: "#home",
+	container: ".home-container",
+	back: {
 		path: "#contact",
 		container: ".contact-container",
-		back: {
-			path: "#projects",
-			container: ".projects-container",},
-		forw: {
-			path: "#home",
-			container: ".home-container"}}	
+	},
+	forw: {
+		path: "#skills",
+		container: ".skills-container"
+	}
+}
+
+var skills = {
+	path: "#skills",
+	container: ".skills-container",
+	back: {
+		path: "#home",
+		container: ".home-container",
+	},
+	forw: {
+		path: "#work",
+		container: ".work-container"
+	}
+}
+
+var work = {
+	path: "#work",
+	container: ".work-container",
+	back: {
+		path: "#skills",
+		container: ".skills-container",
+	},
+	forw: {
+		path: "#projects",
+		container: ".projects-container"
+	}
+}
+
+var projects = {
+	path: "#projects",
+	container: ".projects-container",
+	back: {
+		path: "#work",
+		container: ".work-container",
+	},
+	forw: {
+		path: "#contact",
+		container: ".contact-container"
+	}
+}
+
+var contact = {
+	path: "#contact",
+	container: ".contact-container",
+	back: {
+		path: "#projects",
+		container: ".projects-container",
+	},
+	forw: {
+		path: "#home",
+		container: ".home-container"
+	}
+}
 
 var fadeTimer = 200
 
 var currentPath = home;
 
-var routeChange = function(direction, sectionObj){
-	if(direction == "forward"){
+var routeChange = function (direction, sectionObj) {
+	if (direction == "forward") {
 		$('.section').fadeOut(fadeTimer);
-		setTimeout(function(){
+		setTimeout(function () {
 			$('.section').hide()
 			$(sectionObj.forw.container).show()
 			currentPath = assignCurrentPath(sectionObj.forw.path)
 		}, fadeTimer);
 	} else {
 		$('.section').fadeOut(fadeTimer);
-		setTimeout(function(){
+		setTimeout(function () {
 			$('.section').hide()
 			$(sectionObj.back.container).show()
 			currentPath = assignCurrentPath(sectionObj.back.path)
 		}, fadeTimer);
 	}
-}	
+}
 
-var assignCurrentPath = function(path){
+var assignCurrentPath = function (path) {
 	console.log("assignCurrentPath " + path)
 	if (path == "#home") return home
 	if (path == "#skills") return skills
@@ -83,38 +98,37 @@ var assignCurrentPath = function(path){
 
 }
 
-window.onload =function(){
-	if (testing == false){
+window.onload = function () {
+	if (testing == false) {
 		currentPath = home
 		$('.section').hide();
 		$('.home-container').show();
 	}
 };
 
+document.addEventListener('keydown', function (event) {
 
-document.addEventListener('keydown', function(event){
-
-	if (event.keyCode == 65 || event.keyCode == 68){
+	if (event.keyCode == 65 || event.keyCode == 68) {
 		var direction;
-		if (event.keyCode == 65){
+		if (event.keyCode == 65) {
 			console.log("a")
 			direction = "backward"
-		} else if (event.keyCode == 68){
+		} else if (event.keyCode == 68) {
 			console.log("d");
 			direction = "forward"
 		}
-		
+
 		console.log(currentPath)
 
-		if (currentPath == home){
+		if (currentPath == home) {
 			routeChange(direction, home)
-		} else if (currentPath == skills){
+		} else if (currentPath == skills) {
 			routeChange(direction, skills)
-		} else if (currentPath == work){
+		} else if (currentPath == work) {
 			routeChange(direction, work)
-		} else if (currentPath == projects){
+		} else if (currentPath == projects) {
 			routeChange(direction, projects)
-		} else if (currentPath == contact){
+		} else if (currentPath == contact) {
 			routeChange(direction, contact)
 		}
 
