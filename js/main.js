476dcbabb12678abdefd408172c90eeb60c0bd1f
joshjinlee <joshua_jin_lee@live.com>
2018-11-01 15:13:22 -0400

    finished projects page

--- js/main.js
+++ js/main.js
@@ -35,12 +35,47 @@ var contact = {
 
 var currentPath = home;
 
+var currentProject = 'asdf';
+
 $(window).bind("orientationchange", function(){
 	var orientation = window.orientation;
 	var new_orientation = (orientation) ? 0 : 180 + orientation;
 	$('body').css({"-webkit-transform": "rotate(" + new_orientation + "deg)"});
 	});
 
+
+var changeProject = function(projectsShow){
+	currentProject = projectsShow
+	$('.projects-show').fadeOut(fadeTimer);
+	setTimeout(function(){
+		$('.projects-show').hide()
+		$(projectsShow).show()
+	},fadeTimer);
+}
+	
+var projectsScroll = function(){
+	var margin_top = parseInt($('#client-server-hotspot').css('marginTop'))
+	var csh_top = $('#client-server-hotspot').offset().top + margin_top
+	var csh_bot = csh_top + $('#client-server-hotspot').outerHeight()
+	var quibble_top = $('#quibble').offset().top + margin_top
+	var quibble_bot = quibble_top + $('#quibble').outerHeight()
+	var eumag_top = $('#eumag').offset().top + margin_top
+	var eumag_bot = eumag_top + $('#eumag').outerHeight()
+	var bendshop_top = $('#bendshop').offset().top + margin_top
+	var bendshop_bot = bendshop_top + $('#bendshop').outerHeight()
+	var scrolled = $(window).height()/2
+
+	if (scrolled > csh_top && scrolled < csh_bot && currentProject != "#projects-csh") 
+		changeProject('#projects-csh')
+	else if (scrolled > quibble_top && scrolled < quibble_bot && currentProject != "#projects-quibble")
+		changeProject('#projects-quibble')
+	else if (scrolled > eumag_top && scrolled < eumag_bot && currentProject != "#projects-eumag")
+		changeProject('#projects-eumag')
+	else if (scrolled > bendshop_top && scrolled < bendshop_bot && currentProject != "#projects-bendshop")
+		changeProject('#projects-bendshop')
+}
+
+$('.projects-content-container').scroll(projectsScroll);
 
 var routeChange = function (direction, sectionObj) {
 	if (direction == "forward") {
@@ -68,7 +103,16 @@ var routeChange = function (direction, sectionObj) {
 			$('.nav-selector').css('right', '' + sectionObj.position + 'px')
 		}, fadeTimer);
 	}
+	if (sectionObj.path == projects.path){
+		console.log("currentPath")
+		$('.projects-show').hide()
+		setTimeout(projectsScroll, fadeTimer*7);
+		currentProject = "asdf"
+	}
 }
+
+
+
 
 var assignCurrentPath = function (path) {
 	console.log("assignCurrentPath " + path)
@@ -77,8 +121,8 @@ var assignCurrentPath = function (path) {
 	if (path == "#work") return work
 	if (path == "#projects") return projects
 	if (path == "#contact") return contact
-
 }
+
 
 window.onload = function () {
 	if (testing == false) {
@@ -88,6 +132,7 @@ window.onload = function () {
 	}
 };
 
+
 $('.nav-toggle').click(function(){
 	$('.nav-container').toggleClass('nav-item-active');
 	$('header').toggleClass('header-active');
@@ -95,47 +140,71 @@ $('.nav-toggle').click(function(){
 });
 
 
+
+
+
 $('#nav-home').hover(
 	function () { $('.nav-selector').css('right', '' + home.position + 'px'); },
 	function () { $('.nav-selector').css('right', '' + currentPath.position + 'px') });
-
 $('#nav-skills').hover(
 	function () { $('.nav-selector').css('right', '' + skills.position + 'px'); },
 	function () { $('.nav-selector').css('right', '' + currentPath.position + 'px') });
-
 $('#nav-work').hover(
 	function () { $('.nav-selector').css('right', '' + work.position + 'px'); },
 	function () { $('.nav-selector').css('right', '' + currentPath.position + 'px') });
-
 $('#nav-projects').hover(
 	function () { $('.nav-selector').css('right', '' + projects.position + 'px'); },
 	function () { $('.nav-selector').css('right', '' + currentPath.position + 'px') });
-
 $('#nav-contact').hover(
 	function () { $('.nav-selector').css('right', '' + contact.position + 'px'); },
 	function () { $('.nav-selector').css('right', '' + currentPath.position + 'px') });
 
+
 $('#nav-home').click(function(){
 	routeChange('click', home);
 });
-
 $('#nav-skills').click(function(){
 	routeChange('click', skills);
 });
-
 $('#nav-work').click(function(){
 	routeChange('click', work);
 });
-
 $('#nav-projects').click(function(){
 	routeChange('click', projects);
 });
-
 $('#nav-contact').click(function(){
 	routeChange('click', contact);
 });
 
+
 document.addEventListener('keydown', function (event) {
+	var margin_top = parseInt($('#client-server-hotspot').css('marginTop'))
+	var csh_top = $('#client-server-hotspot').offset().top + margin_top
+	// var csh_bot = csh_top + $('#client-server-hotspot').outerHeight()
+	// var quibble_top = $('#quibble').offset().top + margin_top
+	// var quibble_bot = quibble_top + $('#quibble').outerHeight()
+	// var eumag_top = $('#eumag').offset().top + margin_top
+	// var eumag_bot = eumag_top + $('#eumag').outerHeight()
+	// var bendshop_top = $('#bendshop').offset().top + margin_top
+	// var bendshop_bot = bendshop_top + $('#bendshop').outerHeight()
+
+	if(currentPath == projects){
+		if (event.keyCode == 87){
+
+			// $('.projects-content-container').animate({
+			// 	scrollTop: $("#client-server-hotspot").offset().top
+			// }, 200)
+			$('.projects-content-container').stop().animate({
+				scrollTop: $("#quibble").offset().top
+			}, 200)
+			// $('.projects-content-container').animate({
+			// 	scrollTop: $("#eumag").offset().top
+			// }, 200)
+			// $('.projects-content-container').animate({
+			// 	scrollTop: $("#bendshop").offset().top
+			// }, 200)
+		}	
+	}
 
 	if (event.keyCode == 65 || event.keyCode == 68) {
 		var direction;
@@ -146,8 +215,6 @@ document.addEventListener('keydown', function (event) {
 			console.log("d");
 			direction = "forward"
 		}
-
-		console.log(currentPath)
 
 		if (currentPath == home) {
 			routeChange(direction, home)
@@ -160,6 +227,8 @@ document.addEventListener('keydown', function (event) {
 		} else if (currentPath == contact) {
 			routeChange(direction, contact)
 		}
-
 	}
+
+
+
 });
